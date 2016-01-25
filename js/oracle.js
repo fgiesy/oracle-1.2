859f832d87c74d6206a4f24e9fe019a3433c4b10
Feld Giesy <feld@4sitestudios.com>
2016-01-24 19:57:00 -0500

    Initial Oracle 1.2 Push.
    Pushing up the new Oracle 1.2. Updated .jsons/folder structure. Removed
    superfluous/unused Gulp modules.

---
+++ js/oracle.js
@@ -0,0 +1,14 @@
+//Drupal JS wrapper.
+
+(function ($) {
+    Drupal.behaviors.ad_calendar = {
+        attach: function (context, settings) {}
+    
+      
+      //Standard js/jQuery goes here.
+    
+    }
+});
+
+
+//Nonstandard
